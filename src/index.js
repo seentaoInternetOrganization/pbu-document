7ccbb197037af93f71f48de6d91604cb177bd1bd
AngusChen <angus1105@gmail.com>
2018-01-16 10:11:41 +0800

    默认宽度 1000px

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ import { mapExaminesWithAll } from './components/docUtils'
 const Option = Select.Option
 
 // const docConfigUrl = "https://pbu-public.oss-cn-beijing.aliyuncs.com/webapps/excel_document/document/vdy08h/vdy08h.json"
-// const docConfigUrl = "https://oss-public.seentao.com/webapps/pbu_document/DJY0066/config/djy0066.json";
+const docConfigUrl = "https://oss-public.seentao.com/webapps/pbu_document/DJY0066/config/djy0066.json";
 // const docConfigUrl = "https://oss-public.seentao.com/webapps/pbu_document/DJY0089/config/DJY0089.json";
 // const docConfigUrl = "https://oss-public.seentao.com/webapps/pbu_document/DJY0071/config/DJY0071.json"
 // const docConfigUrl = "https://oss-public.seentao.com/webapps/pbu_document/DJY0064/config/DJY0064.json"
 // const docConfigUrl = "https://oss-public.seentao.com/webapps/pbu_document/DJY0067/config/DJY0067.json"
-const docConfigUrl = "http://39.107.8.110:8080/stest/document.sales";
+// const docConfigUrl = "http://39.107.8.110:8080/stest/document.sales";
 // const docConfigUrl = "https://pbu-public.oss-cn-beijing.aliyuncs.com/webapps/excel_document/document/q4qhej/q4qhej.json"
 // const docConfigUrl = "https://pbu-public.oss-cn-beijing.aliyuncs.com/webapps/excel_document/document/ep001e/ep001e.json"
 
